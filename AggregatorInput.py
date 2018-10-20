89aad00a0d5c9a943f266d103ed47a794adeea68
alexfromsocal <alittaua67@gmail.com>
2018-10-20 14:26:36 -0700

    Flow Comments added for what is needed for the Python interface

--- AggregatorInput.py
+++ AggregatorInput.py
@@ -6,6 +6,14 @@ userSearchQuery = input("Enter a search term....\n")
 
 print(userSearchQuery)
 
+#Now send query to Bing Custom Search API
+#Within Bing Custom Search, we specifically use the Bing News API for relevant data
+#Using a scrapper, take relevant data for our dataset from first ten to twenty articles (this number cna be infinite, use better filters to improve results)
+#Send data to ML reference
+#Take outputted data from ML and put into list
+#Output list in descending order.
+#Stretch goal output: List number of concurring sources and what those sources are
+
 
 
 print("Program done.")
